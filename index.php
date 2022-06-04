14e42b941c001b350998e174911bfb0f8ddbcf64
DaniKef <gurenko888@gmail.com>
2022-06-04 17:09:26 +0300

    commit1

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ru" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>U-Vision</title>
+    <meta name="author" content="Daniil H.">
+    <meta name="description" content="U-Vision - это сайт для удобного просмотра фильмов и сериалов.">
+    <link rel="shortcut icon" type="image/x-icon" href="Media/Images/main_Icon.ico">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="CSS/styleAll.css">
+  </head>
+  <body>
+    <header>
+      <a href="http://u-vision/"><div class="header-bg"></div></a>
+      <nav class="nav"></nav>
+    </header>
+
+    <main>
+      <p>111111 Приветавававава ввас всех hello!! There</p>
+    </main>
+
+    <footer>
+      <div>
+          <p> <a href="http://u-vision/">U-Vision</a> | 2022 | Работает с божьей помощью.</p>
+      </div>
+    </footer>
+
+  </body>
+</html>
